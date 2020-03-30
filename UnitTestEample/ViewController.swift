a9bd5857ea19bb9c5bed39202c8177c3341257b2
Milan Panchal <sam07it22@gmail.com>
2020-03-30 12:51:03 +0530

    feat: Unit test added

--- UnitTestEample/ViewController.swift
+++ UnitTestEample/ViewController.swift
@@ -16,5 +16,10 @@ class ViewController: UIViewController {
     }
 
 
+    // Return product of two int numbers
+    func productOfTwoNumbers(num1: Int, num2: Int) -> Int {
+        return num1 * num2
+    }
+
 }
 
--- UnitTestEampleTests/UnitTestEampleTests.swift
+++ UnitTestEampleTests/UnitTestEampleTests.swift
@@ -26,9 +26,47 @@ class UnitTestEampleTests: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testSumOfTwoNumbers() {
+        
+        let num1 = 10
+        let num2 = 20
+        let sum = num1 + num2
+        
+        XCTAssertEqual(sum, 30, "sum should be 30")
+    }
 
+    func testProductOfTwoNumbers() {
+        
+        let vc = ViewController()
+        let product = vc.productOfTwoNumbers(num1: 2, num2: 3)
+        
+        XCTAssertEqual(product, 6, "product should be 6")
+    }
+
+    func testFetchTODOList() {
+        
+        let exp = expectation(description: "fetching to-do list from server")
+        
+        let session: URLSession = URLSession(configuration: .default)
+        let url = URL(string: "https://jsonplaceholder.typicode.com/todos")
+        
+        // when
+        session.dataTask(with: url!) { data, response, error in
+
+            XCTAssertNil(error)
+            
+            exp.fulfill()
+            
+        }.resume()
+        
+        waitForExpectations(timeout: 10.0) { (error) in
+            print(error?.localizedDescription ?? "error")
+        }
+
+    }
 }
